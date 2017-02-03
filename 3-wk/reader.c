20af461921dcf2125a0b07227b2f27bc464745a8
L. Pesola <lpesola@cs.helsinki.fi>
2017-02-03 17:15:58 +0200

    commented read_line function

--- 3-wk/reader.c
+++ 3-wk/reader.c
@@ -27,16 +27,25 @@ int main(int argc, char *argv[])
 	exit(0);
 }
 
+/*
+ * Read text from stdin. 
+ * Write all lines 100 bytes or less to fifo1 for further processing.
+ * Longer lines are ignored.
+ */
 
 void read_lines(int writefd) 
 {
 	puts("type in text to be doubled, ctrl-D ends");
-	char readbuf[100];
-	char writebuf[100];
+	char readbuf[100]; 
+	char writebuf[100]; 
 	ssize_t nbytes;
-	int len = 0;
-	int longline = 0;
-	int j = 0;
+	// number of characters read before a newline
+	int len = 0; 
+	// non-zero if last character in readbuf wasn't a newline
+	int longline = 0; 
+	// the current position in writebuf
+	int j = 0; 
+
 	while ((nbytes = read(STDIN_FILENO, readbuf, sizeof readbuf)) != 0) {
 		if (nbytes == -1 && errno == EINTR)
 			continue;
@@ -45,13 +54,24 @@ void read_lines(int writefd)
 			exit(1);
 		}
 		
-		// Lines over 100 bytes must be ignored
+/* 
+ * To ignore too long lines keep track of the number of characters read (len).
+ * When >100 characters have been read, set longline to be true (non-zero) 
+ * to mark that next segments first characters are part of a too long line.
+ * 
+ * When a newline is read, if the line was not too long (= longline is false (0)),
+ * write everything up to here to writefd for further processing.
+ * 
+ * If longline is true, the end of a too long line has been reached so 
+ * characters up to here can be ignored. 
+ */
+
 		for (int i = 0; i < nbytes ; i++) {
 			writebuf[j] = readbuf[i];
 			len +=1;
 			j += 1;
 			if (readbuf[i] == '\n') {
-				if (longline == 1) {
+				if (longline) {
 					longline = 0;
 					len = 0;
 					j = 0;	

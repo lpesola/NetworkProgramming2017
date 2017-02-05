a81b1d16d8eb6ce5c00566e87299928ca3d3127a
L. Pesola <lpesola@cs.helsinki.fi>
2017-02-05 13:11:38 +0200

    ex 2

---
+++ 3-wk/shm_incr2.c
@@ -0,0 +1,64 @@
+#define _XOPEN_SOURCE 600
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include "lock_fcntl.c"
+#define FILE_MODE       (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+			/* default permissions for new files */
+#define LOCKFILE       "/tmp/MKLCKXXXXXX" 
+#define MAPPED_FILE "temp.txt"
+void my_lock_init(char *pathname);
+void my_lock_wait();
+void my_lock_release();
+
+/*
+ * ex 2.
+ * modify programs shm_incr.c and lock_fcntl.c so that 
+ * - there's no fork: separate programs are run instead
+ * - the programs access the same shared memory area
+ * => modify 
+ * - file lock init
+ * - shared memory init (processes need to use the same path name)
+ */
+
+int main(int argc, char **argv)
+{
+	int fd, i, nloop, zero = 0;
+	int *ptr;
+	pid_t pid;
+
+	if (argc != 2) {
+		perror("usage: incr2 <#loops>"); exit(1); }
+	nloop = atoi(argv[1]);
+
+		/* 4open file, initialize to 0, map into memory */
+	// first change argv[1] => MAPPED_FILE
+	if ((fd = open(MAPPED_FILE, O_RDWR | O_CREAT, FILE_MODE)) < 0) {
+		perror("open failed"); exit(1); }
+	if (write(fd, &zero, sizeof(int)) <0) {
+		perror("write failed"); exit(1); }
+	ptr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+        if (ptr == MAP_FAILED) {
+		perror("mmap failed"); exit(1); }
+	close(fd);
+
+	pid = getpid();
+	/* 4create, initialize, and unlink file lock */
+	my_lock_init(LOCKFILE);
+
+	setbuf(stdout, NULL);	/* stdout is unbuffered */
+
+	sleep(1);
+	for (i = 0; i < nloop; i++) {
+		my_lock_wait();
+		printf("process %d: %d\n", pid, (*ptr)++);
+		sleep(2);
+		my_lock_release();
+	}
+
+	exit(0);
+}

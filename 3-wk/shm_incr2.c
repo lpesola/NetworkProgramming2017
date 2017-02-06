0b4c8c435622e47eddc53a07651468a23244ae85
L. Pesola <lpesola@cs.helsinki.fi>
2017-02-06 10:34:26 +0200

    ex 2

--- 3-wk/shm_incr2.c
+++ 3-wk/shm_incr2.c
@@ -23,6 +23,12 @@ void my_lock_release();
  * => modify 
  * - file lock init
  * - shared memory init (processes need to use the same path name)
+ * 
+ * ex 3.
+ * modify ex 2. so that there's a shared character array adjacent to the shared
+ * integer counter in the shared memory area.
+ * Every time a process updates the counter, it writes its name in the table
+ * using the counter value as the index
  */
 
 int main(int argc, char **argv)
@@ -32,31 +38,43 @@ int main(int argc, char **argv)
 	pid_t pid;
 
 	if (argc != 2) {
-		perror("usage: incr2 <#loops>"); exit(1); }
+		puts("usage: incr2 <#loops>"); 
+		exit(1); 
+	}
 	nloop = atoi(argv[1]);
 
-		/* 4open file, initialize to 0, map into memory */
-	// first change argv[1] => MAPPED_FILE
+	/* 4open file, initialize to 0, map into memory */
+	// first change for ex 2: argv[1] => MAPPED_FILE
 	if ((fd = open(MAPPED_FILE, O_RDWR | O_CREAT, FILE_MODE)) < 0) {
-		perror("open failed"); exit(1); }
+		perror("open failed"); 
+		exit(1); 
+	}
+	
+	// this is used to initialize counter to 0?
 	if (write(fd, &zero, sizeof(int)) <0) {
-		perror("write failed"); exit(1); }
+		perror("write failed"); 
+		exit(1); 
+	}
+	
 	ptr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-        if (ptr == MAP_FAILED) {
-		perror("mmap failed"); exit(1); }
+
+	if (ptr == MAP_FAILED) {
+		perror("mmap failed"); 
+		exit(1); 
+	}
 	close(fd);
 
 	pid = getpid();
 	/* 4create, initialize, and unlink file lock */
 	my_lock_init(LOCKFILE);
-
 	setbuf(stdout, NULL);	/* stdout is unbuffered */
 
-	sleep(1);
+	sleep(2);
 	for (i = 0; i < nloop; i++) {
 		my_lock_wait();
-		printf("process %d: %d\n", pid, (*ptr)++);
-		sleep(2);
+		int temp = (*ptr)++;
+		printf("process %d: %d\n", pid, temp);
+		usleep(200000);
 		my_lock_release();
 	}
 

5ebc48746b9ab5505c9edff6ddb4848688ba4cbb
L. Pesola <lpesola@cs.helsinki.fi>
2017-02-06 18:00:47 +0200

    ex 5

---
+++ 3-wk/ex5/client.c
@@ -0,0 +1,88 @@
+#define _DEFAULT_SOURCE
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+#include <sys/errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define PORT 50002
+
+void send_data(int writefd, int datasize, int writesize);
+
+/* ex 5.
+ * Modify ex 4. so that
+ * - client doesn't read stdin: instead it generates random data itself
+ * - total amount of bytes to generate is given as an argument
+ * - maximum amount of data to write is given as an argument
+ * - close TCP connection to inform server that all data has been sent
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 4) {
+		puts("usage: ./reader <ipaddr> datasize writesize");
+		exit(1);
+	}
+
+	int datasize = atoi(argv[2]);
+	int writesize = atoi(argv[3]);
+	int sockfd;
+	struct sockaddr_in serv_addr;
+
+	sockfd = socket(PF_INET, SOCK_STREAM, PF_UNSPEC);
+	if (sockfd < 0) {
+		perror("socket");
+		exit(1);
+	}
+
+	bzero((void*)&serv_addr, sizeof(serv_addr));
+
+	serv_addr.sin_family = AF_INET;
+	//convert IP address to correct form
+	inet_aton(argv[1], &serv_addr.sin_addr);
+	serv_addr.sin_port = htons(PORT);
+
+	if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+		perror("connect");
+		exit(1);
+	}
+		
+	send_data(sockfd, datasize, writesize);
+	close(sockfd);
+	shutdown(sockfd, SHUT_WR);
+
+ 	exit(0);
+}
+
+/*
+ * Read text from stdin. 
+ * Write all lines 100 bytes or less to socket for further processing.
+ * Longer lines are ignored.
+ */
+
+void send_data(int writefd, int datasize, int writesize) 
+{
+	char data[datasize]; 
+	ssize_t nbytes;
+
+	// generate random data to send
+	// data doesn't need to be truly random or in any way meaningful
+	srand(10);
+	for (int i = 0; i < datasize; i++) {
+		int random_int = 26 * (rand() / RAND_MAX + 1.0) + 97;
+		data[i] = (char) random_int;	
+	}
+
+	nbytes = write(writefd, &data, writesize);
+	if (nbytes < 0) {
+		perror("write");
+		exit(1);
+	}
+	
+}
+
+
---
+++ 3-wk/ex5/server.c
@@ -0,0 +1,92 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define PORT 50002
+
+
+/* ex 5.
+ * Modify ex 4 so that
+ * - Server ignores all data from the client. 
+ * - Maximum number of bytes to read with a single read is given as an argument.
+ *
+ */
+
+void read_lines(int readfd, int maxbytes);
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		puts("usage ./server maxbytes");
+		exit(0);
+	}
+	int maxbytes = atoi(argv[1]);
+
+	struct sockaddr_in cli_addr, serv_addr;
+	int lstnsock = socket(PF_INET, SOCK_STREAM, PF_UNSPEC);
+	if (lstnsock < 0) {
+		perror("socket");
+		exit(1);
+	}
+
+
+	bzero((void*) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(PORT);
+
+	if(bind(lstnsock, (struct sockaddr *)&serv_addr, sizeof(serv_addr))){
+		perror("bind");
+		exit(1);
+	}
+
+	listen(lstnsock, 2);
+
+	for (;;) {
+		socklen_t clilen = sizeof(cli_addr);
+		int newsockfd = accept(lstnsock, (struct sockaddr*)&cli_addr, &clilen);
+		if (newsockfd < 0) {
+			perror("accept");
+			exit(1);
+		}
+		int pid = fork();
+		if(pid < 0){
+			perror("fork");
+			exit(1);
+		}
+		if (pid == 0) {
+			close(lstnsock);
+			read_lines(newsockfd, maxbytes);
+			exit(0);
+		}
+		close(newsockfd);
+	}
+	exit(0);
+}
+
+void read_lines(int readfd, int maxbytes)
+{	
+
+	int nbytes;
+	char readbuf[maxbytes];
+	while ((nbytes = read(readfd, readbuf, sizeof readbuf)) != 0) {
+		if (nbytes == -1 && errno == EINTR) {
+			continue;
+		}
+		else if (nbytes == -1) {
+			perror("doubler read");
+			exit(1);	
+		}
+
+	}
+	puts("done reading");
+	close(readfd);
+}
+
